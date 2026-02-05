9ae4831cd7e95ea8db4e8c8b2aa9aa3162a6c0fa
EsrakFahim <efahim0656@gmail.com>
2026-02-05 15:41:52 +0600

    update the env and implement cloudinary

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,16 +1,18 @@
 // src/config/multer.ts
 import multer from "multer";
-import path from "path";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "./cloudinary";
 
-const storage = multer.diskStorage({
-      destination: "uploads/",
-      filename: (_req, file, cb) => {
-            const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
-            cb(null, unique + path.extname(file.originalname));
-      },
+const storage = new CloudinaryStorage({
+      cloudinary,
+      params: async (_req, file) => ({
+            folder: "specialists",
+            resource_type: file.mimetype.startsWith("video") ? "video" : "image",
+            public_id: `${Date.now()}-${file.originalname}`,
+      }),
 });
 
 export const upload = multer({
       storage,
-      limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
+      limits: { fileSize: 50 * 1024 * 1024 },
 });
