07362c721d96d9d065b49a1753a16844a41bfba4
EsrakFahim <efahim0656@gmail.com>
2026-02-05 15:23:03 +0600

    feat: Configure database connection using DATABASE_URL with SSL and simplify data source setup.

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,34 +5,19 @@ import { ServiceOffering } from './entities/ServiceOffering';
 import { ServiceOfferingMasterList } from './entities/ServiceOfferingsMasterList';
 import { PlatformFee } from './entities/PlatformFee';
 
-console.log(
-  'DB CONFIG:',
-  {
-    type: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432'),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  }
-)
 export const AppDataSource = new DataSource({
-
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  synchronize: true,
+  url: process.env.DATABASE_URL, // ✅ USE THIS
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: true, // ⚠️ OK for dev, NOT prod
   logging: false,
   entities: [
     Specialist,
     Media,
     ServiceOffering,
     ServiceOfferingMasterList,
-    PlatformFee
+    PlatformFee,
   ],
-  migrations: [],
-  subscribers: [],
 });
